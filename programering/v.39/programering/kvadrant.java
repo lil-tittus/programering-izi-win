35b7ccabf432988a3984b4d2a9354e77f9f4596d
lil-tittus <jared.tittus@elev.ga.ntig.se>
2019-10-03 10:14:21 +0200

    gjort del ett

---
+++ programering/v.39/programering/kvadrant.java
@@ -0,0 +1,5 @@
+package programering;
+
+public class kvadrant {
+
+}
---
+++ programering/v.39/programering/r2.java
@@ -0,0 +1,21 @@
+package programering;
+
+import java.util.Scanner;
+
+public class r2 {
+ public static void main(String[] args) {
+	
+	 Scanner input = new Scanner(System.in);
+		System.out.println("Ange ett värde för S");
+		int s = input.nextInt();
+		
+		System.out.println("Ange ett Värde för R1");
+		int r1 = input.nextInt();
+		
+		System.out.println(2*s-r1);
+		
+
+		
+	 
+}
+}
