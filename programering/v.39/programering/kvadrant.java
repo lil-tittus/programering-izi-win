73219f860a724cfddee448026103e91dc446aa00
lil-tittus <jared.tittus@elev.ga.ntig.se>
2019-10-03 10:26:21 +0200

    gjorde en kvadrant

--- programering/v.39/programering/kvadrant.java
+++ programering/v.39/programering/kvadrant.java
@@ -1,5 +1,34 @@
 package programering;
 
+import java.util.Scanner;
+
 public class kvadrant {
 
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ange ett X värde");
+		int x = input.nextInt();
+		
+		System.out.println("Ange ett Y värde");
+		int y = input.nextInt();
+		
+		if((x > 0) && (y > 0)) {
+			System.out.println("Kvadrant 1");
+		}
+		if((x < 0)&&(y > 0)) {
+		System.out.println("Kvadrant 2");
+	}
+		if((x > 0)&&(y < 0)) {
+			System.out.println("Kvadrant 3");
+		}
+		if((x < 0)&&(y < 0)) {
+			System.out.println("Kvadrant 4");
+			
+		}
+	}
 }
+		
+		
+	
+
