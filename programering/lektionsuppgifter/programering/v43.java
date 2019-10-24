8df4956a8119b632316394bca3d7839b9ec2e765
lil-tittus <jared.tittus@elev.ga.ntig.se>
2019-10-24 10:23:00 +0200

    nu har jag gjort en outptint och en input
    nu har jag gjort en outptint och en input

---
+++ programering/lektionsuppgifter/programering/v43.java
@@ -0,0 +1,26 @@
+package programering;
+
+public class v43 {
+
+	public static void main(String[] args) {
+		System.out.println(volume(5)+"liter");
+		}
+	
+	
+	public static double volume(double radius) {
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
