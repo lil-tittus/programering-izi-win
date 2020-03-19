59b8632c0f96afae1252695f2b0b973fe20ae43b
lil-tittus <jared.tittus@elev.ga.ntig.se>
2020-03-19 17:59:55 +0100

    Klar med första metoden och börjat på andra

---
+++ TalespeletProjekt/src/Spel/Talspelet.java
@@ -0,0 +1,20 @@
+package Spel;
+
+public class Talspelet {
+    public static void main(String[] args) {
+        System.out.println(welcome());
+        System.out.println("Choose a difficulty: \n");
+
+
+
+    }
+
+    public static String welcome() {
+        return "Shalom my friend, welcome to my epic game!";
+    }
+
+    public static int chooseDifficulty() {
+        System.out.println("Easy, Normal, Hard");
+
+    }
+}
