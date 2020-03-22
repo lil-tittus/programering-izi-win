caa37c11361517dfaee1a169db01570f7b96fa6b
lil-tittus <jared.tittus@elev.ga.ntig.se>
2020-03-22 18:50:19 +0100

    Satte in en trycatch men programmet funkar fortfarande inte

--- programering/src/programering/Tal.java
+++ programering/src/programering/Tal.java
@@ -10,6 +10,7 @@ public class Tal {
 	static int Rightanswarmedium;
 	static int Rightanswarhard;
 	static Scanner input = new Scanner (System.in);
+	static int Exeption;
 	
 	public static void main(String[] args) {
 		System.out.println("Shalom my comrade, welcome to my epic game!");
@@ -24,7 +25,7 @@ public class Tal {
 		System.out.println("Difficultys :  \b 1(easy) \b 2(medium) \b 3(hard)");
 		while (true){
 			System.out.println("Choose a difficulty from 1-3");
-			int Choosedifficulty = input.nextInt();
+			Choosedifficulty = Exeption;
 			if ( Choosedifficulty == 1 || Choosedifficulty  == 2 || Choosedifficulty  == 3) {
 				break;
 			
@@ -32,9 +33,23 @@ public class Tal {
 
 	
 		}
-	
+	Random();
 	}
 	
+	
+	 public static int Exception () {
+		 String WrongFromUser = "";
+		 while (true) {
+			 WrongFromUser = input.nextLine();
+			 try {
+				 return Integer.parseInt(WrongFromUser);
+				 
+			 } catch (Exception e) {
+				 System.out.println("Fel input! Du måste skriva siffror!");
+				 input.hasNext();
+			 }
+		 }
+	 }
 	public static void Choosedifficultyeasy() {
 		if(Choosedifficulty == 1) {
 		if(Playerguess < Rightanswareasy) {
@@ -61,6 +76,20 @@ public class Tal {
 		else if(Playerguess == Rightanswarmedium) {
 			System.out.println("You got it my comrade!!");
 		}
+		}
+	}
+	
+	public static void Choosedifficultyhard() {
+		if(Choosedifficulty == 3) {
+		if(Playerguess < Rightanswarhard) {
+			System.out.println("Number is lower");
+			System.out.println("Remaining guesses");
+		}
+		else if(Playerguess > Rightanswarhard) {
+			System.out.println("Number is higher");
+			System.out.println("Remaining guesses");
+		}
+		
 		}
 	}
 	
